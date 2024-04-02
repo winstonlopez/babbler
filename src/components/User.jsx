dfbf3c933a1dbe9c3e10d49e3d30ed44279253d7
winstonlopez <ev.winston2304@gmail.com>
2024-04-02 10:20:25 +0800

    fixed responsive nav

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,10 +4,9 @@ import { useParams } from 'react-router-dom'
 const User = ({  users  }) => {
 
 
-    console.log(users)
     const id = useParams().id
     const person = users.find(u => u.id === id)
-    console.log(person)
+
 
     return ( 
         <div className='userContentContainer'>
